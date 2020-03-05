984d611bf5dc694b1f5a6f82c16b4e4dc3e03c66
Gerson Penha <Gerson Penha@DESKTOP-VREDC3F>
2020-03-05 07:39:17 -0300

    Inclusão da agenda

--- 2020S1-1/src/br/com/app/fatec/App.java
+++ 2020S1-1/src/br/com/app/fatec/App.java
@@ -1,12 +1,39 @@
 package br.com.app.fatec;
 
+import br.com.modelo.fatec.Agenda;
+import br.com.modelo.fatec.Pessoa;
 import br.com.negocio.fatec.AsciiArt;
+import br.com.negocio.fatec.Controle;
 import br.com.negocio.fatec.Menu;
 
 public class App {
 	public static void main(String[] args) {
 		AsciiArt.printAsciiArt("Serasa");
 		Menu menu = new Menu();
-		menu.imprimirMenu();
+		Agenda agenda = new Agenda();
+		int op = 1000;
+		while(op != 0) {
+			menu.imprimirMenu();
+			Controle controle = new Controle();
+			op = controle.opcao();
+			switch (op) {
+			case 1:
+				Pessoa p = new Pessoa("nome");
+				agenda.clientes.add(p);
+				break;
+
+			default:
+				menu.imprimirMenu();
+			}
+		}
+		
 	}
 }
+
+
+
+
+
+
+
+
