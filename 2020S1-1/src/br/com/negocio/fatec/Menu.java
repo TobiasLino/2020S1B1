896afe82beb204eda7f0fd4d9f403c08410a74be
Gerson Penha <Gerson Penha@DESKTOP-VREDC3F>
2020-03-05 07:28:31 -0300

    Alteração no menu

--- 2020S1-1/src/br/com/negocio/fatec/Menu.java
+++ 2020S1-1/src/br/com/negocio/fatec/Menu.java
@@ -3,10 +3,10 @@ package br.com.negocio.fatec;
 public class Menu {
 	public void imprimirMenu() {
 		System.out.println("#### Menu ####");
-		System.out.println("1 - Inserir contato");
-		System.out.println("2 - Listar contatos");
-		System.out.println("3 - Editar contato");
-		System.out.println("4 - Excluir contato");
+		System.out.println("1 - Inserir cliente");
+		System.out.println("2 - Listar cliente");
+		System.out.println("3 - Editar cliente");
+		System.out.println("4 - Excluir cliente");
 		System.out.println("0 - Sair");
 	}
 }
